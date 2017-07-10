bd65d426ce509e1017935b0f3c4a2f9644440813
srinivasu <srinivasdadi9000@gmail.com>
2017-07-10 20:44:50 +0530

    dad

--- app/src/main/java/bestapphome/e_vijayawada/Login.java
+++ app/src/main/java/bestapphome/e_vijayawada/Login.java
@@ -1,34 +1,58 @@
 package bestapphome.e_vijayawada;
 
+import android.animation.AnimatorInflater;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Login extends Activity implements View.OnClickListener {
     Button btn_login;
-    EditText input_usename,input_password;
+    EditText input_usename, input_password;
+    ImageView logo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        btn_login = (Button)findViewById(R.id.btn_login);
-        input_usename = (EditText)findViewById(R.id.input_usename);
-        input_password = (EditText)findViewById(R.id.input_password);
-        btn_login.setOnClickListener(Login.this);
 
+
+
+        btn_login = (Button) findViewById(R.id.btn_login);
+        input_usename = (EditText) findViewById(R.id.input_usename);
+        input_password = (EditText) findViewById(R.id.input_password);
+        btn_login.setOnClickListener(Login.this);
+        logo = (ImageView) findViewById(R.id.applogo);
+      /*  Animation myAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blinking);
+        logo.startAnimation(myAnim);*/
+     /*   ObjectAnimator anim = (ObjectAnimator) AnimatorInflater.loadAnimator(getApplicationContext(), R.animator.flipping);
+        anim.setTarget(logo);
+        anim.setDuration(3000);
+
+        anim.start();*/
+
+        input_usename.setOnClickListener(Login.this);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_login:
-                Intent i = new Intent(getApplicationContext(),home.class);
+                Intent i = new Intent(getApplicationContext(), home.class);
                 startActivity(i);
+                break;
+            case R.id.input_usename:
+                Animation myAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce);
+                input_usename.startAnimation(myAnim);
                 break;
         }
     }
--- app/src/main/java/bestapphome/e_vijayawada/home.java
+++ app/src/main/java/bestapphome/e_vijayawada/home.java
@@ -7,8 +7,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,10 +24,25 @@ public class home extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
         select_option = (Spinner) findViewById(R.id.select_option);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.planets_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         select_option.setAdapter(adapter);
-    }
 
+        select_option.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Animation myAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce);
+                select_option.startAnimation(myAnim);
+
+                Toast.makeText(getBaseContext(), adapter.getItem(position).toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
 }
