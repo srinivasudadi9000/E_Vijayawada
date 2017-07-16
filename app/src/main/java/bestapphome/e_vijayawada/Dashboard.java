6629a085205d0b1519b473fbf323b135fcd6461e
srinivasu <srinivasdadi9000@gmail.com>
2017-07-16 17:58:35 +0530

    dome

--- app/src/main/java/bestapphome/e_vijayawada/Dashboard.java
+++ app/src/main/java/bestapphome/e_vijayawada/Dashboard.java
@@ -1,20 +1,39 @@
 package bestapphome.e_vijayawada;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import bestapphome.e_vijayawada.json.JSONParser;
+
 public class Dashboard extends Activity {
-    ProgressBar progress1,progress2,progress3,progress4,progress5;
-    TextView PendingBefore,PendingAfter,RedressedBefore,RedressedAfter,Rejected;
+    ProgressBar progress1, progress2, progress3, progress4, progress5;
+    TextView PendingBefore, PendingAfter, RedressedBefore, RedressedAfter, Rejected;
     Handler progressHandler = new Handler();
     int i = 0;
+    ProgressDialog progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +50,15 @@ public class Dashboard extends Activity {
         RedressedAfter = (TextView) findViewById(R.id.RedressedAfter);
         Rejected = (TextView) findViewById(R.id.Rejected);
 
-         new Thread(new Runnable() {
+      /*  progress = new ProgressDialog(Dashboard.this);
+        progress.setMessage("Fetching data from server..");
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progress.setIndeterminate(true);
+        progress.setCancelable(false);
+        progress.show();
+*/
+
+        new Thread(new Runnable() {
             public void run() {
                 while (i < 100) {
                     i += 2;
@@ -53,7 +80,7 @@ public class Dashboard extends Activity {
                         Thread.sleep(50);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
-                        Toast.makeText(getBaseContext(),"kasldfkasld",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "kasldfkasld", Toast.LENGTH_SHORT).show();
                     }
 
                 }
@@ -61,6 +88,96 @@ public class Dashboard extends Activity {
             }
         }).start();
 
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences sharedPreferences = getSharedPreferences("Userinfo", MODE_PRIVATE);
+                new Dashboard.getstatus(sharedPreferences.getString("intofficerid", null)).execute();
+                Animation slideUp2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.zoomin);
+                PendingBefore.startAnimation(slideUp2);
+                PendingAfter.startAnimation(slideUp2);
+                RedressedBefore.startAnimation(slideUp2);
+                RedressedAfter.startAnimation(slideUp2);
+                Rejected.startAnimation(slideUp2);
+                //Do something after 100ms
+                Toast.makeText(getBaseContext(),"alskdflasd",Toast.LENGTH_SHORT).show();
+            }
+        }, 3000);
+
 
     }
+
+    private class getstatus extends AsyncTask<String, String, JSONObject> {
+
+        private ProgressDialog pDialog;
+        private ArrayList<NameValuePair> nameValuePairs;
+        private JSONObject json;
+        String id;
+
+        public getstatus(String officerid) {
+            this.id = officerid;
+        }
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... arg0) {
+            nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("intOfficerid", id));
+            json = JSONParser.makeServiceCall("http://208.78.220.51/VMCGMS/GrievanceDashboardService.aspx", 1, nameValuePairs);
+
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            // Toast.makeText(getApplicationContext(), json.toString(), Toast.LENGTH_SHORT).show();
+          //  progress.dismiss();
+            try {
+                JSONArray jsonObject = json.getJSONArray("dashboard");
+                for (int i = 0; i < jsonObject.length(); i++) {
+                    JSONObject value = jsonObject.getJSONObject(i);
+                    //    Toast.makeText(getApplicationContext(), value.getString("intUserid").toString(), Toast.LENGTH_SHORT).show();
+                     PendingBefore.setText(value.getString("PendingBefor")+" %");
+                    PendingAfter.setText(value.getString("PendingAfter")+" %");
+                    RedressedBefore.setText(value.getString("RedressedBefor")+" %");
+                    RedressedAfter.setText(value.getString("RedressedAfter")+" %");
+                    Rejected.setText(value.getString("Rejected")+" %");
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+
+                showalert("Server Busy At This Moment !!","hai");
+            }
+        }
+    }
+
+    void showalert(String alert_msg, final String show) {
+        android.app.AlertDialog.Builder alertDialogBuilder = new android.app.AlertDialog.Builder(Dashboard.this);
+        alertDialogBuilder.setTitle("E_Vijayawada");
+        // alertDialogBuilder.setIcon(R.drawable.aplogo);
+        // set dialog message
+        alertDialogBuilder.setMessage(alert_msg).setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+        alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        // create alert dialog
+        android.app.AlertDialog alertDialog = alertDialogBuilder.create();
+        // show it
+        alertDialog.show();
+    }
+
 }
